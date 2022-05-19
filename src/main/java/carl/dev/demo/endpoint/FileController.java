5c77b998bd51aec7f6784c9ea13cd8833b8fec54
Carlos <mugetsuvita@gmail.com>
2022-05-19 09:46:27 -0300

    rest controller

--- src/main/java/carl/dev/demo/endpoint/FileController.java
+++ src/main/java/carl/dev/demo/endpoint/FileController.java
@@ -6,12 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -20,8 +20,8 @@ import carl.dev.demo.message.ResponseFile;
 import carl.dev.demo.message.ResponseMessage;
 import carl.dev.demo.service.FileStorageService;
 
-@Controller
-@CrossOrigin("http://localhost:8081")
+@RestController
+@RequestMapping(value = "/medias")
 public class FileController {
   @Autowired
   private FileStorageService storageService;
@@ -31,7 +31,7 @@ public class FileController {
     try {
       storageService.store(file);
       message = "Uploaded the file successfully: " + file.getOriginalFilename();
-      return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+      return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseMessage(message));
     } catch (Exception e) {
       message = "Could not upload the file: " + file.getOriginalFilename() + "!";
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
